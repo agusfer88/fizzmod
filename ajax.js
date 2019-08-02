62aa24d41bdbf9357d931871607e0ea3c3568a7f
agus <agusfernandez1988@gmail.com>
2019-08-01 21:54:51 -0300

    ajax

--- ajax.js
+++ ajax.js
@@ -2,8 +2,12 @@ var xhr = new XMLHttpRequest
 
 xhr.addEventListener("readystatechange",()=>{
     console.log(`Nuevo Estado${xhr.readyState}`)
-})
-
+});
+xhr.addEventListener("load",()=>{
+ if (xhr.status == 200){
+    document.body.innerText = xhr.response
+ }
+});
 xhr.open("get","info.txt");
 
 xhr.send()
